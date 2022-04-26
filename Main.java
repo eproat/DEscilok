60293ccacd016300df01e2f8fc4111789b380a2d
Kristian <104433636+eproat@users.noreply.github.com>
2022-04-26 11:57:43 +0000

    a

--- Main.java
+++ Main.java
@@ -1,9 +1,43 @@
 class Main {
-  public static void main(String[] args) {
-    for (int i =1; i <= 2 ; i++)
-            for (int j = 1; j <=3 ; j++)
-                System.out.println(i+ "," +j);{
+  
 
-  }
+        /*
+        /*int sum=0;
+        for (int i = 10; i <=100; i=i+10) {
+            sum = sum+i;
+            System.out.println("summa ir vienāda ar f"+sum);
+
+        }*/
+        /*int count =0;
+        while (count>2) {
+            System.out.println("come");
+            count++;
+        }*/
+      /*  int x=0;
+        do {
+            System.out.println("come");
+        }while (x!=0);
+
+        }*/
+
+  /*      int a = 1234;
+        while (a > 0) {
+            int vieni = a % 10;
+            a = a / 10;
+            System.out.println(vieni);
+        }*/
+        Scanner sc=new Scanner(System.in);
+        System.out.println("vveid sadsadaxzczx");
+        int x=sc.nextInt();
+        int y=1;
+        int z=1;
+        while (y!=x) {
+            z = (z * y);
+            z++;
+        }
+        System.out.println(z);
+
+
+
     }
-}
+    }
